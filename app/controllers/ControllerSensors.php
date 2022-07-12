4d5c28e534aa78b0d21c044665ddfb9f1a23b3ce
Morgan <embeecode@gmail.com>
2022-07-12 21:39:31 +0200

    controllerPage deleted

--- app/controllers/ControllerSensors.php
+++ app/controllers/ControllerSensors.php
@@ -1,12 +1,54 @@
 <?php
 
-class ControllerSensors extends Controller
+final class ControllerSensors extends Controller
 {
     private Sensor $sensor_model;
 
     public function __construct()
     {
         $this->sensor_model = new Sensor();
+    }
+
+    /**
+     * render home page
+     * 
+     * 
+     * 
+     */
+    public function defaultAction(): void
+    {
+        $sensor_model = new Sensor();
+        $chart_model = new Chart();
+
+        $sensors = $sensor_model->wrapAll();
+        $sensors_history = $chart_model->getDbHistory();
+
+        $data = array(
+            'head_title' => "Vos Capteurs",
+            'sensors' => $sensors,
+            'sensors_history' => $sensors_history
+        );
+
+        $this->render('template', 'sensor/home', $data);
+    }
+
+    /**
+     * render edit page
+     * 
+     * 
+     * 
+     */
+    public function editAction(string $id): void
+    {
+        $sensor_model = new Sensor();
+
+        $data = array(
+            'head_title' => "Edition capteur",
+            'sensor' => $sensor_model->wrap($id),
+            'action' => 'update'
+        );
+
+        $this->render('template', 'sensor/edit', $data);
     }
 
     /**
@@ -89,4 +131,21 @@ class ControllerSensors extends Controller
         );
         $this->render('template', 'standard/message', $data);
     }
+
+    /**
+     * render creation page
+     * 
+     * 
+     * 
+     */
+    public function creationAction(): void
+    {
+        $data = array(
+            'head_title' => "Creation capteur",
+            'action' => 'create'
+        );
+
+        $this->render('template', 'sensor/edit', $data);
+    }
+
 }
